8e878066778f4f44bcf894ff11b0896ee8b93e83
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-08-28 17:08:55 +0200

    feat: jwt token logic
    Added the basic token logic needed for authentication

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/m1thrandir225/galore-services
 go 1.21.0
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
---
+++ token/jwt_maker.go
@@ -0,0 +1,68 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+const minSecretSize = 32
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretSize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d characters", minSecretSize)
+	}
+
+	return &JWTMaker{secretKey}, nil
+}
+
+func (maker *JWTMaker) CreateToken(email string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(email, duration)
+
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+
+	return token.SignedString([]byte(maker.secretKey))
+
+}
+
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+
+		return []byte(maker.secretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+}
