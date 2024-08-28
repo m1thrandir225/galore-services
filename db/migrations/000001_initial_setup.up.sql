92c92eb9428516dcb23f52ae013995979a38d0ee
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-08-28 14:15:49 +0200

    feat: initial migration

--- db/migrations/000001_initial_setup.up.sql
+++ db/migrations/000001_initial_setup.up.sql
@@ -6,5 +6,19 @@ CREATE TABLE users (
   avatar_url TEXT NOT NULL, 
   enabled_push_notifications BOOLEAN DEFAULT FALSE NOT NULL,
   enabled_email_notifications BOOLEAN DEFAULT FALSE NOT NULL, 
-  created_at TIMESTAMPTZ DEFAULT 'now()' NOT NULL,
-)
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE flavours (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL UNIQUE, 
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE notification_types (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+  title TEXT NOT NULL UNIQUE, 
+  content TEXT NOT NULL, 
+  tag TEXT NOT NULL, 
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
