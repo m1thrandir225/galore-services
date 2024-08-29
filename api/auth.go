c422b752229b3b51f7afb5f88214e29ecd6a7691
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-08-30 01:03:29 +0200

    feat: working register route

--- api/auth.go
+++ api/auth.go
@@ -7,31 +7,42 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgtype"
 	db "github.com/m1thrandir225/galore-services/db/sqlc"
+	"github.com/m1thrandir225/galore-services/util"
 )
 
-type RegisterUserRequest struct {
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required, email"`
-	Password string `json:"password" binding:"required"`
-	Birthday string `json:"birthday" binding:"required"`
+type registerUserRequest struct {
+	Name      string `json:"name" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	AvatarUrl string `json:"avatar_url" binding:"required"`
+	Password  string `json:"password" binding:"required"`
+	Birthday  string `json:"birthday" binding:"required"`
+}
+
+type registerUserResponse struct {
+	User        db.CreateUserRow `json:"user"`
+	AccessToken string           `json:"access_token"`
 }
 
 type LoginUserRequest struct{}
 
 func (server *Server) registerUser(ctx *gin.Context) {
-	var requestData RegisterUserRequest
+	var requestData registerUserRequest
 
 	if err := ctx.Bind(&requestData); err != nil {
 		ctx.JSON(400, errorResponse(err))
 		return
 	}
+	format := "2006-01-02"
 
-	dateFormat := "2022-03-01"
+	birthdayDate, err := time.Parse(format, requestData.Birthday)
 
-	birthdayDate, err := time.Parse(dateFormat, requestData.Birthday)
+	var dbDate pgtype.Date
 
-	dbDate := pgtype.Date{
-		Time: birthdayDate,
+	err = dbDate.Scan(birthdayDate)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
 	}
 
 	if err != nil {
@@ -39,15 +50,38 @@ func (server *Server) registerUser(ctx *gin.Context) {
 		return
 	}
 
-	//TODO: hash password
+	hashedPassword, err := util.HashPassowrd(requestData.Password)
+
 	args := db.CreateUserParams{
 		Email:     requestData.Email,
 		Birthday:  dbDate,
 		Name:      requestData.Name,
-		AvatarUrl: "",
-		Password:  "",
+		AvatarUrl: requestData.AvatarUrl,
+		Password:  hashedPassword,
 	}
 
+	newEntry, err := server.store.CreateUser(ctx, args)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	accessToken, err := server.tokenMaker.CreateToken(args.Email, server.config.AccessTokenDuration)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	//TODO: implement adding file to storage
+
+	response := registerUserResponse{
+		User:        newEntry,
+		AccessToken: accessToken,
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
 
 func (server *Server) loginUser(ctx *gin.Context) {
