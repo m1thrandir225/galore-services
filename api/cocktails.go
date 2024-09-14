2c77a512bb98e05116529e02fc45ba1d57e783ed
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-09-14 17:41:51 +0200

    fix: added error handling to unhandled scenarios

--- api/cocktails.go
+++ api/cocktails.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"encoding/json"
+	"mime/multipart"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
@@ -9,17 +12,15 @@ import (
 	"github.com/m1thrandir225/galore-services/dto"
 	"github.com/m1thrandir225/galore-services/token"
 	"github.com/m1thrandir225/galore-services/util"
-	"mime/multipart"
-	"net/http"
 )
 
 type CreateOrUpdateCocktailRequest struct {
-	Name         string                `form:"name" binding:"required"`
-	IsAlcoholic  bool                  `form:"isAlcoholic" binding:"required"`
-	Glass        string                `form:"glass" binding:"required"`
 	Image        *multipart.FileHeader `form:"file" binding:"required"`
-	Instructions string                `form:"instructions" json:"instructions" binding:"required"`
-	Ingredients  string                `form:"ingredients" json:"ingredients" binding:"required"`
+	Name         string                `form:"name" binding:"required"`
+	Ingredients  string                `form:"ingredients" binding:"required"`
+	Glass        string                `form:"glass" binding:"required"`
+	Instructions string                `form:"instructions" binding:"required"`
+	IsAlcoholic  bool                  `form:"isAlcoholic" binding:"required"`
 }
 
 func (server *Server) createCocktail(ctx *gin.Context) {
@@ -34,7 +35,6 @@ func (server *Server) createCocktail(ctx *gin.Context) {
 	}
 
 	err := isAlcoholic.Scan(requestData.IsAlcoholic)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
@@ -50,9 +50,8 @@ func (server *Server) createCocktail(ctx *gin.Context) {
 		return
 	}
 
-	//Unload the bytes from the uploaded file
+	// Unload the bytes from the uploaded file
 	fileData, err := util.BytesFromFile(requestData.Image)
-
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
@@ -67,9 +66,8 @@ func (server *Server) createCocktail(ctx *gin.Context) {
 	}
 	payload := data.(*token.Payload)
 
-	//Upload the file to the public/user_id/file
+	// Upload the file to the public/user_id/file
 	filePath, err := server.storage.UploadFile(fileData, payload.ID.String(), requestData.Image.Filename)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
@@ -84,7 +82,6 @@ func (server *Server) createCocktail(ctx *gin.Context) {
 	}
 
 	cocktail, err := server.store.CreateCocktail(ctx, arg)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
@@ -113,28 +110,25 @@ func (server *Server) deleteCocktail(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	//2. Get the cocktail data
+	// 2. Get the cocktail data
 	cocktail, err := server.store.GetCocktail(ctx, cocktailId)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	//3. Delete the associated image
+	// 3. Delete the associated image
 	err = server.storage.DeleteFile(cocktail.Image)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	//4. Delete the cocktail itself
+	// 4. Delete the cocktail itself
 	err = server.store.DeleteCocktail(ctx, cocktailId)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 	ctx.Status(http.StatusOK)
-
 }
 
 func (server *Server) getCocktail(ctx *gin.Context) {
@@ -146,21 +140,18 @@ func (server *Server) getCocktail(ctx *gin.Context) {
 	}
 
 	cocktailId, err := uuid.Parse(uriData.ID)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
 	cocktail, err := server.store.GetCocktail(ctx, cocktailId)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
 	ctx.JSON(http.StatusOK, cocktail)
-
 }
 
 func (server *Server) updateCocktail(ctx *gin.Context) {
@@ -191,30 +182,33 @@ func (server *Server) updateCocktail(ctx *gin.Context) {
 	}
 
 	err := isAlcoholic.Scan(requestData.IsAlcoholic)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
 
 	cocktailId, err := uuid.Parse(uriData.ID)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
 	cocktail, err := server.store.GetCocktail(ctx, cocktailId)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
 	newImageData, err := util.BytesFromFile(requestData.Image)
-
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
 	newFilePath, err := server.storage.ReplaceFile(cocktail.Image, newImageData)
-
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 	arg := db.UpdateCocktailParams{
 		ID:           cocktailId,
 		Name:         requestData.Name,
@@ -226,7 +220,6 @@ func (server *Server) updateCocktail(ctx *gin.Context) {
 	}
 
 	updated, err := server.store.UpdateCocktail(ctx, arg)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
