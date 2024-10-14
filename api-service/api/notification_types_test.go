2e43ebf6fdb3b2daa23f9e44e2bac92d4d4a19d4
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-10-14 22:21:40 +0200

    feat: file created for notification_type tests

---
+++ api-service/api/notification_types_test.go
@@ -0,0 +1,11 @@
+package api
+
+import "testing"
+
+//TODO: write the tests for the notification types api
+
+func TestCreateNotificationTypeApi(t *testing.T) {}
+func TestGetNotificationTypesApi(t *testing.T)   {}
+func TestGetNotificationTypeApi(t *testing.T)    {}
+func TestDeleteNotificationTypeApi(t *testing.T) {}
+func TestUpdateNotificationTypeApi(t *testing.T) {}
