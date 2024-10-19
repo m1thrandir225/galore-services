85053aa7d5b28783a49d01d87968a1acb796e0ce
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-10-19 17:02:29 +0200

    progress: user tests (cont.)

--- api-service/api/users_test.go
+++ api-service/api/users_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 	mockstorage "github.com/m1thrandir225/galore-services/storage/mock"
 	"io"
-	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -246,9 +245,9 @@ func TestUpdateUserDetailsApi(t *testing.T) {
 
 	arg := db.UpdateUserInformationParams{
 		ID:        user.ID,
-		Name:      util.RandomString(12),
+		Name:      "James Brown",
 		AvatarUrl: avatarUrl,
-		Email:     util.RandomEmail(),
+		Email:     "jamesbrown@gmail.com",
 		Birthday:  newDate,
 	}
 
@@ -261,7 +260,7 @@ func TestUpdateUserDetailsApi(t *testing.T) {
 		checkResponse func(recorder *httptest.ResponseRecorder)
 	}{
 		{
-			name:   "OK",
+			name:   "OK-NoFile",
 			userId: user.ID.String(),
 			body: gin.H{
 				"id":       user.ID,
@@ -273,21 +272,36 @@ func TestUpdateUserDetailsApi(t *testing.T) {
 				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, time.Minute)
 			},
 			buildStubs: func(store *mockdb.MockStore, storage *mockstorage.MockFileService) {
+				gomock.InOrder(
+					store.EXPECT().GetUser(gomock.Any(), arg.ID).Times(1).Return(user, nil),
+					//storage.EXPECT().ReplaceFile(gomock.Any(), gomock.Any()).Times(1).Return("", nil),
+					store.EXPECT().UpdateUserInformation(gomock.Any(), arg).Times(1).Return(db.UpdateUserInformationRow{}, nil),
+				)
 
-				store.EXPECT().GetUser(gomock.Any(), arg.ID).Times(1).Return(user, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name:   "OK-NoFile",
+			userId: user.ID.String(),
+			body: gin.H{
+				"id":       user.ID,
+				"name":     arg.Name,
+				"email":    arg.Email,
+				"birthday": newDateString,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore, storage *mockstorage.MockFileService) {
+				gomock.InOrder(
+					store.EXPECT().GetUser(gomock.Any(), arg.ID).Times(1).Return(user, nil),
+					//storage.EXPECT().ReplaceFile(gomock.Any(), gomock.Any()).Times(1).Return("", nil),
+					store.EXPECT().UpdateUserInformation(gomock.Any(), arg).Times(1).Return(db.UpdateUserInformationRow{}, nil),
+				)
 
-				store.EXPECT().UpdateUserInformation(gomock.Any(), arg).Times(1).Return(db.UpdateUserInformationRow{
-					ID:                        user.ID,
-					Name:                      arg.Name,
-					Email:                     arg.Email,
-					CreatedAt:                 user.CreatedAt,
-					Birthday:                  arg.Birthday,
-					EnabledPushNotifications:  user.EnabledPushNotifications,
-					EnabledEmailNotifications: user.EnabledEmailNotifications,
-					AvatarUrl:                 arg.AvatarUrl,
-				}, nil)
-
-				storage.EXPECT().ReplaceFile(gomock.Any(), gomock.Any()).Times(1).Return("", nil)
 			},
 			checkResponse: func(recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusOK, recorder.Code)
@@ -299,45 +313,131 @@ func TestUpdateUserDetailsApi(t *testing.T) {
 		testCase := testCases[i]
 
 		t.Run(testCase.name, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-
-			body := new(bytes.Buffer)
-			writer := multipart.NewWriter(body)
-
-			_ = writer.WriteField("email", testCase.body["email"].(string))
-			_ = writer.WriteField("name", testCase.body["name"].(string))
-			_ = writer.WriteField("birthday", testCase.body["birthday"].(string))
-
-			fileWriter, err := writer.CreateFormFile("avatar_url", avatarUrl)
-			require.NoError(t, err)
-			_, err = fileWriter.Write([]byte(util.RandomString(512)))
-			require.NoError(t, err)
-
-			err = writer.Close()
-			require.NoError(t, err)
-
-			url := fmt.Sprintf("/api/v1/users/%s", testCase.userId)
-			request, err := http.NewRequest(http.MethodPost, url, body)
-			require.NoError(t, err)
-
-			request.Header.Set("Content-Type", writer.FormDataContentType())
-
-			store := mockdb.NewMockStore(ctrl)
-			storage := mockstorage.NewMockFileService(ctrl)
-			testCase.buildStubs(store, storage)
-
-			server := newTestServer(t, store, nil, storage)
-			recorder := httptest.NewRecorder()
-
-			testCase.setupAuth(t, request, server.tokenMaker)
-			server.router.ServeHTTP(recorder, request)
-			testCase.checkResponse(recorder)
+			//TODO: finish implementing this
+			//ctrl := gomock.NewController(t)
+			//defer ctrl.Finish()
+			//
+			//body := new(bytes.Buffer)
+			//writer := multipart.NewWriter(body)
+			//
+			//_ = writer.WriteField("email", testCase.body["email"].(string))
+			//_ = writer.WriteField("name", testCase.body["name"].(string))
+			//_ = writer.WriteField("birthday", testCase.body["birthday"].(string))
+			//if testCase.name == "OK-WithFile" {
+			//	fileWriter, err := writer.CreateFormFile("avatar_url", avatarUrl)
+			//	require.NoError(t, err)
+			//	_, err = fileWriter.Write([]byte(util.RandomString(512)))
+			//	require.NoError(t, err)
+			//}
+			//
+			//err := writer.Close()
+			//require.NoError(t, err)
+			//
+			//url := fmt.Sprintf("/api/v1/users/%s", testCase.userId)
+			//request, err := http.NewRequest(http.MethodPost, url, body)
+			//require.NoError(t, err)
+			//
+			//request.Header.Set("Content-Type", writer.FormDataContentType())
+			//
+			//store := mockdb.NewMockStore(ctrl)
+			//storage := mockstorage.NewMockFileService(ctrl)
+			//testCase.buildStubs(store, storage)
+			//
+			//server := newTestServer(t, store, nil, storage)
+			//recorder := httptest.NewRecorder()
+			//
+			//testCase.setupAuth(t, request, server.tokenMaker)
+			//server.router.ServeHTTP(recorder, request)
+			//testCase.checkResponse(recorder)
 		})
 	}
 }
 
-func TestUpdateUserPasswordApi(t *testing.T) {}
+func TestUpdateUserPasswordApi(t *testing.T) {
+	user := randomUser(t)
+
+	testCases := []struct {
+		name          string
+		userId        string
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:   "OK",
+			userId: user.ID.String(),
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.UpdateUserPasswordParams{
+					ID:       user.ID,
+					Password: util.RandomString(10),
+				}
+				store.EXPECT().UpdateUserPassword(gomock.Any(), arg).Times(1).Return(nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name:   "Unauthorized",
+			userId: user.ID.String(),
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().UpdateUserPassword(gomock.Any(), gomock.Any()).Times(0).Return(nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name:   "Not Found",
+			userId: user.ID.String(),
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.UpdateUserPasswordParams{
+					ID:       user.ID,
+					Password: util.RandomString(10),
+				}
+				store.EXPECT().UpdateUserPassword(gomock.Any(), arg).Times(1).Return(sql.ErrNoRows)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name:   "Bad Request",
+			userId: user.ID.String(),
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.UpdateUserPasswordParams{
+					ID:       user.ID,
+					Password: util.RandomString(10),
+				}
+				store.EXPECT().UpdateUserPassword(gomock.Any(), arg).Times(1).Return(sql.ErrNoRows)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name: "Internal Server Error",
+		},
+	}
+
+	for i := range testCases {
+		testCase := testCases[i]
+
+		t.Run(testCase.name, func(t *testing.T) {
+		})
+	}
+}
 
 func TestUpdateUserPushNotificationsApi(t *testing.T) {}
 
